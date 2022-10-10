636ff8059144197ce34295bcab5a00bea60dc925
Tomasz Mrowicki <tomrowicki@gmail.com>
2022-10-10 17:02:25 +0200

    Day 2 Part 1 solved

--- day2/day2.go
+++ day2/day2.go
@@ -1,9 +1,58 @@
 package day2
 
-import "fmt"
-
 func FindDoublesTriplesAndMultiplyOccurences(boxIDs []string) int64 {
 
-	fmt.Println(boxIDs)
-	return 1
+	doubles := 0
+	triples := 0
+
+	for _, id := range boxIDs {
+		// check if the id has a double
+		if hasDoubleLetter(id) {
+			doubles++
+		}
+		// check if the id has a tripe
+		if hasTripleLetter(id) {
+			triples++
+		}
+	}
+	return int64(doubles * triples)
 }
+
+func hasDoubleLetter(id string) bool {
+	for _, char := range id {
+		charCount := countOccurencesOfChar(char, id)
+		if charCount == 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasTripleLetter(id string) bool {
+	for _, char := range id {
+		charCount := countOccurencesOfChar(char, id)
+		if charCount == 3 {
+			return true
+		}
+	}
+	return false
+}
+
+func countOccurencesOfChar(givenChar rune, id string) int {
+	count := 0
+
+	for _, char := range id {
+		if char == givenChar {
+			count++
+		}
+	}
+	return count
+}
+
+//func establishUniqueRunes(id string) []rune {
+//	mapOfRunes := make(map[rune]int)
+//
+//	for pos, char := range "日本語" {
+//		fmt.Printf("character %c starts at byte position %d\n", char, pos)
+//	}
+//}
--- runner.go
+++ runner.go
@@ -5,13 +5,14 @@ import (
 	"AdventOfCode2018/common"
 	"AdventOfCode2018/day1"
 	"AdventOfCode2018/day2"
+	"fmt"
 )
 
 func main() {
 	//result := d1p1()
 	//fmt.Println("Result: ", result)
 	//fmt.Println("Francesk's solution: ", CampD1P1())
-	d2p1()
+	fmt.Println("D2P1: ", d2p1())
 }
 
 func d1p1() int64 {
@@ -22,6 +23,7 @@ func d1p2() int64 {
 	return day1.Rollfrequenciesbeforevaluerepeats(common.InputLinesToSlice("day1/day1_input.txt"))
 }
 
-func d2p1() {
-	day2.FindDoublesTriplesAndMultiplyOccurences(common.InputLinesToSlice("day2/day2_input.txt"))
+func d2p1() int64 {
+	// should be 5390
+	return day2.FindDoublesTriplesAndMultiplyOccurences(common.InputLinesToSlice("day2/day2_input.txt"))
 }
